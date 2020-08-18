55e4e35298e4dfb587e342a2c417d04aa07d83d7
marina budz <budz.marina97@gmail.com>
2020-08-18 13:52:20 +0300

    chnages
    changes

--- sign-up/src/components/sucess/sucess.js
+++ sign-up/src/components/sucess/sucess.js
@@ -1,17 +1,29 @@
 import React from "react";
 import "./sucess.scss";
-
-const Sucess = ({ name, lastName }) => {
+const Sucess = ({ firstName, lastName, signedUp }) => {
+  if (!signedUp) {
+    return (
+      <div className="main">
+        <div className="success">
+          <h2> Congratulations!</h2>
+          <h4> You have successfully Logged In</h4>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="main">
       <div className="success">
         <h2> Congratulations!</h2>
         <h4> You have successfully Signed up</h4>
         <h3>
-          Welcome {name} {lastName}
+          Welcome
+          <p>{firstName}</p>
+          <p>{lastName}</p>
         </h3>
       </div>
     </div>
   );
 };
+
 export default Sucess;
